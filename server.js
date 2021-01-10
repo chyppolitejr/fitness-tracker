a26e7107d1a81c86828c3147e391705210f2dec5
chyppoli <claude.hyppolite@gmail.com>
2021-01-09 20:39:05 -0500

    deploying

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/fitnessTrackerDB",
   {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex:true,
     useFindAndModify: false,
   }
 );
